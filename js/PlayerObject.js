b0841a43b66bd5aa1010db26c4c51535a4082075
ethanstockton <e.stockton-2020@hull.ac.uk>
2022-11-28 16:06:38 +0000

    teams

--- js/PlayerObject.js
+++ js/PlayerObject.js
@@ -7,7 +7,45 @@ class PlayerObject {
         this.playershown;
         this.marker;// = pmarker;
         this.check;
+        this.team = 0;
+        this.teamset = 0;
     }
+
+    
+
+    //spectatoricon = new LeafIcon({iconUrl: 'images/marker-icon-speclightblue.png'});
+    //redteamicon = new LeafIcon({iconUrl: 'images/marker-icon-teamred.png'});
+    //blueteamicon = new LeafIcon({iconUrl: 'images/marker-icon-teamblue.png'});
+
+    spectatoricon = L.icon({
+        iconUrl: 'images/marker-icon-speclightblue.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 40],
+        popupAnchor: [0, -20],
+        //shadowUrl: 'my-icon-shadow.png',
+        //shadowSize: [68, 95],
+        //shadowAnchor: [22, 94]
+    });
+
+    redteamicon = L.icon({
+        iconUrl: 'images/marker-icon-teamred.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 40],
+        popupAnchor: [0, -20],
+        //shadowUrl: 'my-icon-shadow.png',
+        //shadowSize: [68, 95],
+        //shadowAnchor: [22, 94]
+    });
+
+    blueteamicon = L.icon({
+        iconUrl: 'images/marker-icon-teamblue.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 40],
+        popupAnchor: [0, -20],
+        //shadowUrl: 'my-icon-shadow.png',
+        //shadowSize: [68, 95],
+        //shadowAnchor: [22, 94]
+    });
 
     setid(pid){
         this.id = pid;
@@ -50,6 +88,16 @@ class PlayerObject {
     getcheck(){
         return this.check;
     }
+
+    setteam(pteam){
+        if(this.teamset == 0){
+            this.team = pteam;
+            this.teamset = 1;
+        }
+    } 
+    getteam(){
+        return this.team;
+    }
     
 
     hideplayer(map){
@@ -75,35 +123,51 @@ class PlayerObject {
     }
 */
     setlocation(pmap ,locationlat, locationlong){
-        if(locationlat == null){
-            locationlat = 0;
-        }
-        if(locationlong == null){
-            locationlong = 0;
-        }
+        //only create the marker when the player has chosen a team
+        //if(this.teamset == 1){
+            if(locationlat == null){
+                locationlat = 0;
+            }
+            if(locationlong == null){
+                  locationlong = 0;
+             }
 
-        if(this.marker != null){
-        
+            if(this.marker != null){
 
-            //var location2 = location.replace('[', '');
-            //var location3 = location2.replace(']', '');
-            //var location4 = location3.split(',');
-
-            this.marker.setLatLng([ locationlat, locationlong]);
-        }
-        else{
-            //var location2 = location.replace('[', '');
-            //var location3 = location2.replace(']', '');
-            //var location4 = location3.split(',');
+                this.marker.setLatLng([ locationlat, locationlong]);
+            }
+            else{
             
-            var user2 = L.marker([ locationlat,locationlong]);
+            
+                //var user2;
+                //red = 1,blue = 2
+                if(this.team == 1){
+                    var user2 = L.marker([ locationlat,locationlong],{icon: this.redteamicon});
+                    user2.bindPopup("<b>" + this.playername + "</b>").openPopup();
 
-            user2.bindPopup("<b>" + this.playername + "</b>").openPopup();
+                    user2.addTo(pmap);
 
-            user2.addTo(pmap);
+                    this.marker = user2;
+                }
+                if(this.team == 2){
+                    var user2 = L.marker([ locationlat,locationlong],{icon: this.blueteamicon});
+                    user2.bindPopup("<b>" + this.playername + "</b>").openPopup();
 
-            this.marker = user2;
-        }
+                    user2.addTo(pmap);
+
+                    this.marker = user2;
+                }
+                
+
+                //user2.bindPopup("<b>" + this.playername + "</b>").openPopup();
+
+                //user2.addTo(pmap);
+
+                //this.marker = user2;
+
+            
+            }
+        //}
        
     }
 }
